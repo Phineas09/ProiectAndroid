d9b235a16fa37d9bd280dab756be30f9228e6406
Phineas09 <claudiu.ghenea15@yahoo.ro>
2021-04-03 00:44:44 +0300

    :tada: Added first version of the project !
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

---
+++ app/src/main/java/ro/mta/proiect/GlobalFunctions.java
@@ -0,0 +1,34 @@
+package ro.mta.proiect;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GlobalFunctions {
+
+    /**
+     * method is used for checking valid email id format.
+     *
+     * @param email
+     * @return boolean true for valid false for invalid
+     */
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+
+    public static String getStringDigest(String string) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] digest = md.digest(string.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digest.length; i++) {
+            sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+
+}
